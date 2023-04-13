36c4f3184167d80cdb4413eb9054e82c1222349e
marwan darwish <darawish663@gmail.com>
2023-04-13 02:43:14 +0200

    added about

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Main from "./components/Main"
-
+import About from"./components/About"
 function App() {
   return (
     <>
@@ -9,7 +9,7 @@ function App() {
       </header>
 
       <Main />
-
+<About/>
     </>
   )
 }
